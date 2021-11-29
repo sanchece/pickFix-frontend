b665adc856ebbe692aefb10b929e6ffbf94d1d2f
carlos sanchez <sanchece98@gmail.com>
2021-11-29 17:48:25 -0500

    added mountUser in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes/Routes";
+import Navbar from "./routes/Navbar";
+import useLocalStorage from "./customHooks/useLocalStorage";
+import jwt from "jsonwebtoken";
+
+import pickFixApi from "./api";
 function App() {
+  const [token, setToken] = useLocalStorage("token");
+  const [currentUser, setCurrentUser] = useState(null);
+
+
+  
+  useEffect(
+    function mountUser(){
+      if(token){
+        console.log("if token:", token)
+        let {email}= jwt.decode(token);
+        pickFixApi.token = token;
+        
+        console.log("in mountUser api token: ",pickFixApi.token)
+
+        
+      }
+      else{
+        console.log("no token")
+      }
+    }
+  )
+
+  async function signUp(data){
+    try{
+      let res= await pickFixApi.signUp(data);
+      setToken(res);
+      return {success:true};
+    }
+    catch(err){
+      return{success:false,err}
+    }
+  }
+  async function logIn(data){
+    try{
+      let res= await pickFixApi.logIn(data);
+
+      setToken(res);
+      return {success:true};
+    }
+    catch(err){
+      return{success:false,err}
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes signUp={signUp} logIn={logIn} />
+    </BrowserRouter>
+
   );
 }
 
